db5de248ea59a7e0d198f4c37f6542971d3cea62
denis240997 <hamitov.97@mail.ru>
2023-03-17 18:17:47 +0600

    Refactoring to add Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import MainFrame from "./MainFrame";
 import NavBar from "./NavBar";
 import content from '/src/content/content.json';
+import { Route, Routes } from "react-router-dom"
 
 
 const App = () => {
     return (
         <div className="app">
             <NavBar />
-            <MainFrame goals={ content.goals }/>
+            <div className="pageContent">
+                <Routes>
+                    <Route path="/" element={<MainFrame goals={content.goals} />} />
+                </Routes>
+            </div>
         </div>
     )
 }
