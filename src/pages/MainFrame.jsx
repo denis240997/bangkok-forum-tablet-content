b800fe3cba14d957a739b5e00a5a867573ccbb5d
denis240997 <hamitov.97@mail.ru>
2023-03-20 03:52:25 +0600

    Main page layout

--- src/pages/MainFrame.jsx
+++ src/pages/MainFrame.jsx
@@ -3,11 +3,21 @@ import './styles/MainFrame.css'
 
 
 const MainFrame = ({ goals }) => {
+
+    const mainTitle = "moscow: current accomplishments and prospects for achieving the sustainable development goals"
+
     return (
         <div className="mainFrame">
-            {
-                goals.map((goal) => (<GoalCard goal={ goal } key={ goal.number }/>))
-            }
+            <div className="contentColumn">
+                <div className="mainTitle">
+                    {mainTitle.toUpperCase()}
+                </div>
+                <div className="goalCardsContainer">
+                    {
+                        goals.map((goal) => (<GoalCard goal={goal} key={goal.number} />))
+                    }
+                </div>
+            </div>
         </div>
     )
 };
