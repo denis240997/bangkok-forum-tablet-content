7cb3d117a2466cd07427f1e3780cfa8ad5198ed1
denis240997 <hamitov.97@mail.ru>
2023-03-20 04:50:43 +0600

    Goal card

--- src/components/GoalCard.jsx
+++ src/components/GoalCard.jsx
@@ -4,22 +4,26 @@ import './styles/GoalCard.css'
 
 const GoalCard = ({ goal }) => {
     return (
-        <div className="goalCard" id={`goal_${goal.number}`}>
+        <Link
+            to="/goal/"
+            state={goal.categories}
+            className="goalCard"
+            id={`goal_${goal.number}`}
+        >
+
             <div className="cardNameContainer">
                 <div className="goalNumber">{goal.number}</div>
                 <div className="goalName">{goal.name.toUpperCase()}</div>
             </div>
-            <Link to="/goal/" state={ goal.categories }>
-                <div className="cardImgContainer">
-                    <img
-                        className="goalLogo"
-                        src={goal.logo}
-                        height={500}
-                        width={500}
-                    />
-                </div>
-            </Link>
-        </div>
+
+            <div className="cardImgContainer">
+                <img
+                    className="goalLogo"
+                    src={goal.logo}
+                />
+            </div>
+
+        </Link>
     )
 };
 
