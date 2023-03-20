46b6e7db4a7e1641ce96dbf47f998164097a6180
denis240997 <hamitov.97@mail.ru>
2023-03-21 00:11:05 +0600

    Layout grid refactor

--- src/pages/PageLayout.jsx
+++ src/pages/PageLayout.jsx
@@ -4,14 +4,10 @@ import './styles/PageLayout.css'
 const PageLayout = ({ title, children }) => {
     return (
         <div className="pageFrame">
-            <div className="contentColumn">
-                <div className="pageTitle">
-                    {title.toUpperCase()}
-                </div>
-                <div className="pageContainer">
-                    {children}
-                </div>
+            <div className="pageTitle">
+                {title.toUpperCase()}
             </div>
+            {children}
         </div>
     )
 };
