2cf03453dce16f919bc7797926b57aa123848867
denis240997 <hamitov.97@mail.ru>
2023-03-21 01:08:55 +0600

    Subgoal card

--- src/components/SubGoalCard.jsx
+++ src/components/SubGoalCard.jsx
@@ -1,10 +1,12 @@
 import './styles/SubGoalCard.css'
+import ArrowIcon from '/src/icons/Arrow.svg'
 
 
 const SubGoalCard = ({ subgoal }) => {
     return (
         <div className="subGoalCard">
             <div className="subGoalName">{subgoal.name.toUpperCase()}</div>
+            <img src={ArrowIcon} alt="--->" />
         </div>
     )
 };
