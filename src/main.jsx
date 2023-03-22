bf0b4e2bb9964143b6756bb2eed607c288e860cd
denis240997 <hamitov.97@mail.ru>
2023-03-23 00:06:42 +0600

    Scroll to top on new page

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import ScrollToTop from './components/ScrollToTop'
 import App from './App'
 import './index.css'
 import { BrowserRouter } from "react-router-dom"
@@ -8,6 +9,7 @@ import { BrowserRouter } from "react-router-dom"
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
+      <ScrollToTop />
       <App />
     </BrowserRouter>
   </React.StrictMode>,
