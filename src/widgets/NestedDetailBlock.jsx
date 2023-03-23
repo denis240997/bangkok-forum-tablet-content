ec47143f26daccb7993da0d730422379a541cc2e
denis240997 <hamitov.97@mail.ru>
2023-03-23 16:01:39 +0600

    Nested detail expand state refactoring

--- src/widgets/NestedDetailBlock.jsx
+++ src/widgets/NestedDetailBlock.jsx
@@ -5,21 +5,32 @@ import "./styles/NestedDetailBlock.css"
 
 const NestedDetailBlock = ({ children, title }) => {
 
+    const [state, setState] = useState({expand: false});
+
+    const EXPAND_STATE = { transform: "scaleY(-1)" }
+    const [arrowStyle, setarrowStyle] = useState({});
+
     const HIDDEN_STATE = { display: "none" }
     const [style, setStyle] = useState(HIDDEN_STATE);
+
     const changeState = () => {
-        style.display ? setStyle({}) : setStyle(HIDDEN_STATE)
+        if (state.expand) {
+            setStyle(HIDDEN_STATE)
+            setarrowStyle({})
+        } else {
+            setStyle({})
+            setarrowStyle(EXPAND_STATE)
+        }
+        setState({expand: !state.expand})
     };
 
     return (
-        <div className="nestedDetailBlock">
-            <div className="nestedDetailTitle" onClick={() => changeState()}>
-                <h3>{title.toUpperCase()}</h3>
-                <img src={DropDownLogo} />
-            </div>
+        <div className="nestedDetailBlock" onClick={() => changeState()}>
+            <h3>{title.toUpperCase()}</h3>
             <div className="nestedDetailContent" style={style} onClick={() => changeState()}>
                 {children}
             </div>
+            <img className='dropDownArrow' src={DropDownLogo} style={arrowStyle} />
         </div>
     );
 }
