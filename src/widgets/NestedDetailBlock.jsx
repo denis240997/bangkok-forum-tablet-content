44e3e95335b2fde5193d6b32c901a5b237792a84
denis240997 <hamitov.97@mail.ru>
2023-03-24 15:07:19 +0600

    Project categories styles

--- src/widgets/NestedDetailBlock.jsx
+++ src/widgets/NestedDetailBlock.jsx
@@ -1,14 +1,14 @@
-import DropDownLogo from '/src/icons/Dropdown.svg'
+import PlusCrossLogo from '/src/icons/PlusCross.svg'
 import { useState } from 'react';
 import "./styles/NestedDetailBlock.css"
 
 
 const NestedDetailBlock = ({ children, title }) => {
 
-    const [state, setState] = useState({expand: false});
+    const [state, setState] = useState({ expand: false });
 
-    const EXPAND_STATE = { transform: "scaleY(-1)" }
-    const [arrowStyle, setarrowStyle] = useState({});
+    const EXPAND_STATE = { transform: "rotate(45deg)" }
+    const [crossStyle, setCrossStyle] = useState({});
 
     const HIDDEN_STATE = { display: "none" }
     const [style, setStyle] = useState(HIDDEN_STATE);
@@ -16,21 +16,23 @@ const NestedDetailBlock = ({ children, title }) => {
     const changeState = () => {
         if (state.expand) {
             setStyle(HIDDEN_STATE)
-            setarrowStyle({})
+            setCrossStyle({})
         } else {
             setStyle({})
-            setarrowStyle(EXPAND_STATE)
+            setCrossStyle(EXPAND_STATE)
         }
-        setState({expand: !state.expand})
+        setState({ expand: !state.expand })
     };
 
     return (
-        <div className="nestedDetailBlock" onClick={() => changeState()} >
-            <h3>{title.toUpperCase()}</h3>
+        <div className="nestedDetailBlock" >
+            <div className="dropDownButtonArea" onClick={() => changeState()} >
+                <h3>{title.toUpperCase()}</h3>
+                <img className='dropDownArrow' src={PlusCrossLogo} style={crossStyle} />
+            </div>
             <div className="nestedDetailContent" style={style} >
                 {children}
             </div>
-            <img className='dropDownArrow' src={DropDownLogo} style={arrowStyle} />
         </div>
     );
 }
