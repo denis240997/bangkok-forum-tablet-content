12ddfa41a58e69138c3b2be9ce8ea598a6cb866c
denis240997 <hamitov.97@mail.ru>
2023-03-27 03:42:25 +0600

    Slider dots fix

--- src/components/SliderModal.jsx
+++ src/components/SliderModal.jsx
@@ -1,8 +1,7 @@
 import Slider from "react-slick";
-import "./styles/SliderModal.css"
-
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import "./styles/SliderModal.css"
 
 
 const range = (start, end) => {
